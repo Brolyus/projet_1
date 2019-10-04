cb396196eebe82ce3f5cd0acc4eed5b84d90c442
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-10-04 09:27:42 +0200

    ajout du javascript: modal pour les events. Ajout d'une nouvelle photo pour le haut de page

---
+++ script.js
@@ -0,0 +1,47 @@
+// First Event's Modal
+let openModal = document.getElementById('open-modal');
+let modal = document.getElementById('modal');
+
+
+openModal.addEventListener('click', () => {
+  modal.style.display ='block';
+});
+
+let closeModal = document.getElementById('close-modal');
+
+
+closeModal.addEventListener('click', () => {
+  modal.style.display ='none';
+});
+
+// Second Event's Modal
+let openModal2 = document.getElementById('open-modal2');
+let modal2 = document.getElementById('modal2');
+
+
+openModal2.addEventListener('click', () => {
+  modal2.style.display ='block';
+});
+
+let closeModal2 = document.getElementById('close-modal2');
+
+
+closeModal2.addEventListener('click', () => {
+  modal2.style.display ='none';
+});
+
+// Third Event's Modal
+let openModal3 = document.getElementById('open-modal3');
+let modal3 = document.getElementById('modal3');
+
+
+openModal3.addEventListener('click', () => {
+  modal3.style.display ='block';
+});
+
+let closeModal3 = document.getElementById('close-modal3');
+
+
+closeModal3.addEventListener('click', () => {
+  modal3.style.display ='none';
+});
