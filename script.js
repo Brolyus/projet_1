15c2932f36c2e5988ff14209f8d32623f1a257ec
Brolyus <desousanthony@gmail.com>
2019-10-04 11:16:55 +0200

    v1.51, résolution bug menu burger

--- script.js
+++ script.js
@@ -49,21 +49,15 @@ const scroll = ScrollReveal({
     duration:1500,
     reset:true
     });
-  
-  // scroll.reveal ('div');
-  
+    
   scroll.reveal ('h1'); 
   
   scroll.reveal ('h2');
   
-  scroll.reveal ('nav');
-  
   scroll.reveal ('header');
   
   scroll.reveal ('footer'); 
-  
-  // scroll.reveal ('p', {delay:100});
-  
+   
   scroll.reveal ('h2');
    
   scroll.reveal ('.photos_resid') 
