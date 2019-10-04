8d19e6d349ab39d666f209ac04d043259c24d700
Christopher Poncelet <christopher.poncelet51@gmail.com>
2019-10-04 09:32:51 +0200

    ajout js

---
+++ script.js
@@ -0,0 +1,24 @@
+const scroll = ScrollReveal({
+    duration:1500,
+    reset:true
+    });
+  
+  scroll.reveal ('div');
+  
+  scroll.reveal ('h1'); 
+  
+  scroll.reveal ('h2');
+  
+  scroll.reveal ('nav');
+  
+  scroll.reveal ('header');
+  
+  scroll.reveal ('footer'); 
+  
+  scroll.reveal ('p', {delay:100});
+  
+  scroll.reveal ('h2');
+   
+  scroll.reveal ('.photos_resid') 
+  
+  
